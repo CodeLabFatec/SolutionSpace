a716d6b90fe5299484d1d2dad692d8b7ab1c83b8
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-03-18 18:59:13 -0300

    chore: add jest config to frontend

---
+++ packages/frontend/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['<rootDir>/node_modules'],
+  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
