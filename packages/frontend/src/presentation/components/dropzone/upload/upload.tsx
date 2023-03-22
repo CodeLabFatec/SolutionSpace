837679f5eb0ae35097a156d576302e239d47b8a8
thaleskerber <thaleskerber@gmail.com>
2023-03-22 11:23:27 -0300

    dropzone

---
+++ packages/frontend/src/presentation/components/dropzone/upload/upload.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useCallback } from 'react'
+import Dropzone, { useDropzone, Accept } from 'react-dropzone'
+import UploadMessage2, { DropContainer, UploadMessage } from './upload-styles'
+
+const Upload: React.FC<{ onUpload: any }> = (props) => {
+  const renderDragMessage = (isDragActive: boolean, isDragReject: boolean) => {
+    if (!isDragActive) {
+      return <UploadMessage2 success={true} texto='Arraste arquivos aqui...'></UploadMessage2>
+    }
+    if (isDragReject) {
+      return <UploadMessage2 success={false} texto='Arquivo não suportado'></UploadMessage2>
+    }
+    return <UploadMessage2 success={true} texto='Solte os arquivos aqui'></UploadMessage2>
+  }
+
+  return (
+    <Dropzone
+      accept={{ 'image/jpeg': ['.jpeg', '.png'], 'video/mp4': ['.mp4', '.MP4'], 'video/mkv': ['.mkv'] }}
+      onDropAccepted={props.onUpload}
+    >
+      {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+        <DropContainer {...getRootProps()} isDragActive={isDragActive} isDragReject={isDragReject}>
+          <input {...getInputProps()} />
+          {renderDragMessage(isDragActive, isDragReject)}
+        </DropContainer>
+      )}
+    </Dropzone>
+  )
+}
+
+export default Upload
--- packages/frontend/src/presentation/pages/ticket-form/ticket-form.tsx
+++ packages/frontend/src/presentation/pages/ticket-form/ticket-form.tsx
@@ -2,12 +2,14 @@ import { Header } from '@/presentation/components'
 import './ticket-form-styles.css'
 
 import React from 'react'
+import DropZone from '@/presentation/components/dropzone/dropzone'
 
 const TicketForm: React.FC = () => {
   return (
     <div>
       <Header />
       <div>TicketForm Page</div>
+      <DropZone />
     </div>
   )
 }
