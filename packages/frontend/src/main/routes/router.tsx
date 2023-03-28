b013c5db42603780c2a8076d10861377d5e1fa52
DiasVitoria <vitoriadias384@gmail.com>
2023-03-28 09:18:50 -0300

    chore/clb-52-analise-de-risco

--- packages/frontend/src/main/routes/router.tsx
+++ packages/frontend/src/main/routes/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import { FormularioChamados, HomeSolicitantes } from '@/presentation/pages'
 import AlinhamentoEstrategico from '@/presentation/pages/alinhamentoEstrategico/alinhamentoEstrategico'
+import AnaliseRisco from '@/presentation/pages/analiseRisco/analiseRisco'
 
 const Router: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const Router: React.FC = () => {
         <Route path='/formularioChamados' element={<FormularioChamados />} />
         <Route path='/home' element={<HomeSolicitantes />} />
         <Route path='/alinhamentoEstrategico' element={<AlinhamentoEstrategico />} />
+        <Route path='/analiserisco' element={<AnaliseRisco />} />
       </Routes>
     </BrowserRouter>
   )
